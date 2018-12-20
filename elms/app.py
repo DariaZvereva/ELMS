f4022d90b08ec48c100e29681226be473c6b43ef
DariaZvereva <zverevads@gmail.com>
2018-12-20 21:41:50 +0300

    init db with schema

---
+++ elms/app.py
@@ -0,0 +1,68 @@
+import sqlite3
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+
+# конфигурация
+DATABASE = '/tmp/lms.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+app = Flask(__name__)
+app.config.from_object(__name__)
+import logging
+
+# Загружаем конфиг по умолчанию и переопределяем в конфигурации часть
+# значений через переменную окружения
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'lms.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Соединяет с указанной базой данных."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def get_db():
+    """Если ещё нет соединения с базой данных, открыть новое - для
+    текущего контекста приложения
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+def init_db():
+    logging.info("Init DB")
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=4)
+    init_db()
+    app.run()
