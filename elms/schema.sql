31fb4ce7fd5c898c0e6d5ae36b0f95af599b1b74
DariaZvereva <zverevads@gmail.com>
2018-12-20 22:03:13 +0300

    new schema

--- elms/schema.sql
+++ elms/schema.sql
@@ -2,6 +2,7 @@ drop table if exists users;
 drop table if exists groups;
 drop table if exists teachers;
 drop table if exists students;
+drop table if exists group_students;
 create table users (
   id integer primary key autoincrement,
   username text not null unique,
@@ -24,7 +25,8 @@ create table teachers (
 create table groups (
   id integer primary key autoincrement,
   name text not null,
-  grade text not null
+  grade text not null,
+  department text not null
 );
 create table students (
   id integer primary key autoincrement,
@@ -35,5 +37,11 @@ create table students (
   education_form text not null,
   education_basis text not null,
   foreign key (user_id) references users(id)
+  foreign key (group_id) references groups(id)
+);
+create table group_students (
+  student_id integer unique,
+  group_id integer not null,
+  foreign key (student_id) references students(id)
   foreign key (group_id) references groups(id)
 );
